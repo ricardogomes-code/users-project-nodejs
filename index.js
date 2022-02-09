281306c6094b7b0fa9adb8dc4ecbc3118cadd3c5
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-09 21:04:23 +0000

    N04 - switch request url

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-const http = require ('http');
+const http= require('http');
 
 let server = http.createServer((req, res) => {
-	console.log('URL:', req.url);
-	console.log('METHOD:', req.method);
 
-	res.end('OK');
+    console.log('URL:', req.url);
+    console.log('METHOD:', req.method);
+
+    switch (req.url) {
+
+        case '/':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'text/html');
+        res.end('<h1>Olá</h1>');
+
+        break;
+
+        case '/users':
+
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+		
+		let usersStringify = JSON.stringify({
+            users:[{
+                name: 'Ricardo',
+                email: 'ricardo@gmail.com',
+                id: 1
+            }]
+        });
+
+        res.end(usersStringify);
+
+        break;
+
+    }
+
 });
 
 server.listen(3000, '127.0.0.1', () => {
 
-	console.log("servidor rodando!");
+    console.log("servidor rodando!");
+
 });
