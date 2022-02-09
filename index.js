6e48e35d2d58561436eaf5464f4889c3bc654f36
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-09 19:59:00 +0000

    N03 - http create server

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("hello world");
+const http = require ('http');
+
+let server = http.createServer((req, res) => {
+	console.log('URL:', req.url);
+	console.log('METHOD:', req.method);
+
+	res.end('OK');
+});
+
+server.listen(3000, '127.0.0.1', () => {
+
+	console.log("servidor rodando!");
+});
