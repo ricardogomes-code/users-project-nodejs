931ebec3e4c869267f7a8eee2a8672b3ef29b2bd
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-10 19:15:15 +0000

    N10 - nedb database

--- routes/users.js
+++ routes/users.js
@@ -1,3 +1,9 @@
+let NeDB = require('nedb');
+let db = new NeDB({
+    filename: 'users.db',
+    autoload: true
+});
+
 module.exports = app => {
 
     app.get('/users', (req, res) => {
@@ -16,13 +22,20 @@ module.exports = app => {
         res.json(users);
     });
     
-    app.post('/users/', (req, res) => {
+    app.post('/users', (req, res) => {
     
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-    
-        let users = req.body;
-        
-        res.json(users);
+        db.insert(req.body, (err, user) => {
+
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                });
+
+            } else {
+
+                res.status(200).json(user);
+            }
+        })
     });
 }
