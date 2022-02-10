e3133379d9699cd214b98d9d48cf000a68badd96
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-10 23:04:56 +0000

    N13 - get user by id

--- routes/users.js
+++ routes/users.js
@@ -7,21 +7,30 @@ let db = new NeDB({
 module.exports = app => {
 
     let route = app.route('/users');
+    let routeId = app.route('/users/:id');
 
     route.get((req, res) => {
 
-        db.find({}).sort({ name: 1 }).exec((err, users) => {
+        db.find({}).sort({ name: 1 }).exec((err, user) => {
 
             if (err) {
                 app.utils.error.send(err, req, res);
 
             } else {
-                res.statusCode = 200;
-                res.setHeader('Content-Type', 'application/json');
+                res.status(200).json(user);
+            }
+        })
+    });
 
-                res.json({
-                    users
-                })
+    routeId.get((req, res) => {
+
+        db.findOne({ _id: req.params.id }).exec((err, user) => {
+
+            if (err) {
+                app.utils.error.send(err, req, res);
+
+            } else {
+                res.status(200).json(user);
             }
         })
     });
