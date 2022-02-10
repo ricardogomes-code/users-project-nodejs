aed34224a102ef4fced20b0a6c4fd4a126c1433c
Ricardo Gomes <ricardogoncalvesgomes@gmail.com>
2022-02-10 19:28:41 +0000

    N11 - GET from nedb

--- routes/users.js
+++ routes/users.js
@@ -8,22 +8,27 @@ module.exports = app => {
 
     app.get('/users', (req, res) => {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-    
-        let users = ({
-            users: [{
-                name: 'Ricardo',
-                email: 'ricardo@gmail.com',
-                id: 1
-            }]
-        });
-        
-        res.json(users);
+        db.find({}).sort({ name: 1 }).exec((err, users) => {
+
+            if (err) {
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error: err
+                })
+
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+
+                res.json({
+                    users
+                })
+            }
+        })
     });
-    
+
     app.post('/users', (req, res) => {
-    
+
         db.insert(req.body, (err, user) => {
 
             if (err) {
